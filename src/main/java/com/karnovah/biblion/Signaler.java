e300ee846eb1067b8d2896bee957bcb1422d3dec
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-20 16:02:08 -0700

    Init observer carrier model

---
+++ src/main/java/com/karnovah/biblion/Signaler.java
@@ -0,0 +1,17 @@
+package com.karnovah.biblion;
+
+public class Signaler {
+	private String content;
+
+	public Signaler() {
+
+	}
+
+	public Signaler(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+}
