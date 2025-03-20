6229eb9608426bd3d6d88b01abb4cb140f5d4346
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-20 15:58:53 -0700

    Init main application

--- src/main/java/com/karnovah/biblion/BiblionApplication.java
+++ src/main/java/com/karnovah/biblion/BiblionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BiblionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BiblionApplication.class, args);
 	}
