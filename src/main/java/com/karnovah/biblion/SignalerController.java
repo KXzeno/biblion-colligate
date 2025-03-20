fb6e9494d9b0c0306e9340cc2fa36fca925f3bcb
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-20 16:02:30 -0700

    Init message-handling controller

---
+++ src/main/java/com/karnovah/biblion/SignalerController.java
@@ -0,0 +1,19 @@
+package com.karnovah.biblion;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class SignalerController {
+
+  @MessageMapping("/observe")
+  @SendTo("/temp/audits")
+  public Signaler signaler(Observer message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Signaler("Hello, " + 
+        HtmlUtils.htmlEscape(message.getSighted()) + "!");
+  }
+
+}
