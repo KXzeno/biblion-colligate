762a3066a711aaf3e0af513fcace8601c24489fe
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-20 17:14:53 -0700

    refactor: change websocket endpoint

--- src/main/java/com/karnovah/biblion/WebSocketConfig.java
+++ src/main/java/com/karnovah/biblion/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/biblion-colligate-websocket");
+    registry.addEndpoint("/colligate-websocket");
   }
 }
