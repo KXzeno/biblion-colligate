9007a69cb6ad50b80f8da1fbbe9712c332da5d50
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-23 17:15:14 -0700

    feat: implement dynamic websocket destinations

--- src/main/java/com/karnovah/biblion/SignalerController.java
+++ src/main/java/com/karnovah/biblion/SignalerController.java
@@ -1,5 +1,6 @@
 package com.karnovah.biblion;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,9 +9,9 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class SignalerController {
 
-  @MessageMapping("/observe")
-  @SendTo("/temp/audits")
-  public Signaler signaler(Observer message) throws Exception {
+  @MessageMapping("/observe/{id}")
+  @SendTo("/temp/audits/{id}")
+  public Signaler signaler(@DestinationVariable String id, Observer message) throws Exception {
     Thread.sleep(1000); // simulated delay
     return new Signaler("Received: " + HtmlUtils.htmlEscape(message.getSighted()));
   }
