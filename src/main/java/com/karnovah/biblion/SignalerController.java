5a2937de3410d5eb2c591f07b5f722ec335a2986
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-24 16:57:01 -0700

    feat: add signal counter and append to signaler
    + Use format method for concatenation

--- src/main/java/com/karnovah/biblion/SignalerController.java
+++ src/main/java/com/karnovah/biblion/SignalerController.java
@@ -8,11 +8,12 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class SignalerController {
-
+  public int signalCount = 0;
   @MessageMapping("/observe/{id}")
   @SendTo("/temp/audits/{id}")
   public Signaler signaler(@DestinationVariable String id, Observer message) throws Exception {
+    this.signalCount++;
     Thread.sleep(1000); // simulated delay
-    return new Signaler("Received: " + HtmlUtils.htmlEscape(message.getSighted()));
+    return new Signaler(String.format("%s%s %s", "Received: ", HtmlUtils.htmlEscape(message.getSighted()), signalCount);
   }
 }
