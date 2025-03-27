17287f5a42da469d2bc05b978ba500ef82935461
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-26 18:47:31 -0700

    refactor: retrieve variables via POJO

--- src/main/java/com/karnovah/biblion/WebSocketConfig.java
+++ src/main/java/com/karnovah/biblion/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.karnovah.biblion;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,18 +10,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableConfigurationProperties(WebSocketProperties.class)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  // TODO: Swap routes with application properties
+  @Autowired
+  private final WebSocketProperties props;
+
+  public WebSocketConfig(WebSocketProperties props) {
+    this.props = props;
+  }
+
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/temp");
-    config.setApplicationDestinationPrefixes("/app");
+    config.enableSimpleBroker(props.getBrokerPrefix());
+    config.setApplicationDestinationPrefixes(props.getAppPrefix());
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/colligate-websocket")
-      .setAllowedOrigins("http://localhost:3000");
+    registry.addEndpoint(props.getEndpoint())
+      .setAllowedOrigins(props.getAllowedOrigins());
   }
 }
