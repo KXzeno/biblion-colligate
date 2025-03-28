e8b90bafc31bf39f528c744f818e59fd1c4eff3a
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-03-27 18:33:59 -0700

    feat: implement connection event handlers
    + Track # of connections as reference for executing on client

--- src/main/java/com/karnovah/biblion/SignalerController.java
+++ src/main/java/com/karnovah/biblion/SignalerController.java
@@ -1,14 +1,19 @@
 package com.karnovah.biblion;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class SignalerController {
   public int signalCount = 0;
+  public int connections = 0;
+
   @MessageMapping("/observe/{id}")
   @SendTo("/temp/audits/{id}")
   public Signaler signaler(@DestinationVariable String id, Observer message) throws Exception {
@@ -20,12 +25,22 @@ public class SignalerController {
       String rate = Character.toString(lastChar);
       int updatedRate = Integer.parseInt(rate);
 
-      return new Signaler(String.format("%s%s %s", "Received: ", HtmlUtils.htmlEscape(msg), signalCount + updatedRate));
+      return new Signaler(String.format("%s %s:%s", "Received:", connections, signalCount + updatedRate);
     } catch (NumberFormatException exc) {
       System.out.printf("%s", exc);
       return new Signaler(String.format("%s%s: %s", "Received: ", HtmlUtils.htmlEscape(msg), "DOES NOT CONFORM TO DATA MODEL"));
     } finally {
       this.signalCount++;
     }
+  }
+
+  @EventListener
+  public void handleConnectEvent(SessionConnectEvent event) {
+    this.connections++;
+  }
+
+  @EventListener
+  public void handleDisconnectEvent(SessionDisconnectEvent event) {
+    this.connections--; 
   }
 }
