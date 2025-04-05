42f9e5cfba1d71892f85a996b897bea487ed617d
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-04-04 19:41:04 -0700

    feat: tag incoming sessions for separate rates

--- src/main/java/com/karnovah/biblion/SignalerController.java
+++ src/main/java/com/karnovah/biblion/SignalerController.java
@@ -9,12 +9,14 @@ import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.util.HtmlUtils;
 
+import com.karnovah.biblion.utils.ImpotentList;
+import com.karnovah.biblion.utils.TaggedSession;;
+
 @Controller
 public class SignalerController {
   public int signalCount = 0;
   public int connections = 0;
-  // TODO: Along with connection count, add sessionIds. If same user,
-  // append :[] length to end
+  private ImpotentList sessions = new ImpotentList();
 
   @MessageMapping("/observe/{id}")
   @SendTo("/temp/audits/{id}")
@@ -23,12 +25,22 @@ public class SignalerController {
     String msg = message.getSighted();
 
     try {
+      TaggedSession session = sessions.find(id);
+
+      if (session == null) {
+        TaggedSession newSession = new TaggedSession(id, 0);
+        sessions.add(newSession);
+        session = newSession;
+      }
+
       char lastChar = msg.charAt(msg.length() - 1);
       String rate = Character.toString(lastChar);
       String user = msg.substring(0, msg.length() - 2);
-      int updatedRate = Integer.parseInt(rate);
+      int updatedRate = session.getRate() + Integer.parseInt(rate);
 
-      return new Signaler(String.format("%s: %s-%s", user, connections, signalCount + updatedRate));
+      session.setRate(updatedRate);
+
+      return new Signaler(String.format("%s: %s-%s", user, connections, updatedRate));
     } catch (NumberFormatException exc) {
       System.out.printf("%s", exc);
       return new Signaler(String.format("%s%s: %s", "Received: ", HtmlUtils.htmlEscape(msg), "DOES NOT CONFORM TO DATA MODEL"));
